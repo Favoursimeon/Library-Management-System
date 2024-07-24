1da7d2f9d34ca7a67e850c96a88aa963e6567329
Favoursimeon <113795312+Favoursimeon@users.noreply.github.com>
2024-07-24 03:28:09 +0000

    Delete client/node_modules directory

--- client/node_modules/antd/lib/spin/Indicator/Progress.d.ts
+++
@@ -1,6 +0,0 @@
-import * as React from 'react';
-export interface ProgressProps {
-    prefixCls: string;
-    percent: number;
-}
-export default function Progress({ percent, prefixCls }: ProgressProps): React.JSX.Element | null;
